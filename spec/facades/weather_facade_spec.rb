399cd8cb4c96112112c6738d92e5bf538008204f
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-08 05:17:44 -0700

    fix typo in test

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe WeatherFacade, :vcr do
   describe 'Facade methods' do
-    it '.location returns a location poro of the location provided' do
-      location = WeatherFacade.weather(38.89, -77.01)
-      expect(location).to be_a(Weather)
+    it '.weather returns a weather poro of the location provided' do
+      weather = WeatherFacade.weather(38.89, -77.01)
+      expect(weather).to be_a(Weather)
     end
   end
 end
