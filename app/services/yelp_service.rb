fe9951df320d8d834ecd5df17ab7882c24693a4e
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-08 09:06:35 -0700

    Correct class name

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,4 +1,4 @@
-class WeatherService < BaseService
+class YelpService < BaseService
   def self.get_search_results(lat, lng)
     response = conn_yelp.get("/businesses/search?latitude=#{lat}&longitude=#{lng}")
     json = get_json(response)
