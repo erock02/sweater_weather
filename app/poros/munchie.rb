53ebd9f89dea4d1279b7d304aef8d5722e206cc0
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-08 10:18:51 -0700

    update munchie poro and serializer

--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -2,6 +2,20 @@ class Munchie
 
   def initialize(restaurant_data)
     @id = "null"
-    binding.pry
+    @destination_city = "#{restaurant_data[:city]}, #{restaurant_data[:state]}"
+    @restaurant = {restaurant: {
+      name: restaurant_data[:businesses][0][:name],
+      address: restaurant_data[:businesses][0][:location][:display_address][0]
+      }
+    }
+  end
+
+  def forecast
+    @location = MapFacade.location(params[@destination_city])
+    @weather = WeatherFacade.weather(@location.latitude, @location.longitude)
+    current_array = {
+      summary: @weather.current_weather[:description],
+      temperature: @weather.current_weather[:temp]
+    }
   end
 end
--- app/serializers/munchie_serializer.rb
+++ app/serializers/munchie_serializer.rb
@@ -1,4 +1,4 @@
 class MunchieSerializer
   include JSONAPI::Serializer
-  
+  attributes :destination_city, :forecast, :restaurant
 end
