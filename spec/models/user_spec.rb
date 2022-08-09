ad5f71de584eeb2e84e244bdb36d662a4b40e862
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-09 03:06:58 -0700

    create user contoller, serializer, and update model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,5 @@ describe User, type: :model do
   describe "validations" do
     it {should validate_uniqueness_of :email}
     it {should have_secure_password}
-    it {should validate_presence_of :password_digest}
   end
 end
