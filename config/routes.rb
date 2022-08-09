1701d661e08f3fb3f046825c8b12c85b48703d8f
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-09 05:06:09 -0700

    finish creating session endpoint for user validation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :users, only: [:new, :create]
+      resources :sessions, only: [:create]
     end
   end
 end
