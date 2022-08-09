90032e69f4b7d639009396a961b8e9367af41188
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-09 09:35:45 -0700

    update test for new get_route method in map_service

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -13,5 +13,12 @@ RSpec.describe MapService, :vcr do
       expect(json_return).to be_a(Array)
       expect(json_return.first[:locations].first[:latLng]).to be_a(Hash)
     end
+
+    it '.get_route returns a json for given two locations' do
+      json_return = MapService.get_route("Denver,CO", "Aspen,CO")
+      expect(json_return).to be_a(Hash)
+      expect(json_return).to have_key(:route)
+      expect(json_return[:route]).to have_key(:formattedTime)
+    end
   end
 end
