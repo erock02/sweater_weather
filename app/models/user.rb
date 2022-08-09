e64ce23dc6981ecc144782ba7cc119053c7c21d4
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-09 09:38:58 -0700

    refactor api_key assignment so that the api key does not change with every call

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  before_create :assign_api_key
+
   validates :email, :presence => true, :uniqueness => { :case_sensitive => true }
   validates_presence_of :password
   validates_confirmation_of :password
   has_secure_password
-
-  def api_key
-    SecureRandom.hex
-  end
+  
+  private
+    def assign_api_key
+      self.api_key = SecureRandom.hex
+    end
 end
