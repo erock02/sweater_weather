f02533bf46d6c39ce564a7d440138c14293a9f02
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-09 09:39:38 -0700

    update user controller to allow user to have api_key saved

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,6 +15,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation, :api_key)
   end
 end
