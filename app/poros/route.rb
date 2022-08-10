66eae018017d97bb75beac9595da923c515ba521
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-10 10:16:50 -0700

    update route to include information when the route is impossible

--- app/poros/route.rb
+++ app/poros/route.rb
@@ -8,34 +8,42 @@ class Route
 
   def initialize(route_data, from, to)
     @id = "null"
-    @travel_time = "#{route_data[:route][:formattedTime][0..1]} hours, #{route_data[:route][:formattedTime][3..4]} minutes"
-    @hours = route_data[:route][:formattedTime][0..1].to_i % 24
-    @days = route_data[:route][:formattedTime][0..1].to_i / 24
+    @impossible_route = route_data[:info][:statuscode] == 402
+    if @impossible_route
+      @travel_time = "impossible"
+    else
+      @travel_time = "#{route_data[:route][:formattedTime][0..1]} hours, #{route_data[:route][:formattedTime][3..4]} minutes"
+      @hours = route_data[:route][:formattedTime][0..1].to_i % 24
+      @days = route_data[:route][:formattedTime][0..1].to_i / 24
+    end
     @start_city = from
     @end_city = to
   end
 
   def weather_at_eta
-    @location = MapFacade.location(@end_city)
-    @weather = WeatherFacade.weather(@location.latitude, @location.longitude)
-    if @hours == 0
-    {
-      temperature: @weather.current_weather[:temperature],
-      conditions: @weather.current_weather[:conditions]
-    }
-    elsif @hours < 8
-      {
-        temperature: @weather.hourly_weather[@hours][:temperature],
-        conditions: @weather.hourly_weather[@hours][:conditions]
-      }
-    elsif @days < 4
-      {
-        temperature: @weather.daily_weather[@days][:max_temp],
-        conditions: @weather.daily_weather[@days][:conditions]
-      }
+    if @impossible_route
+      ""
     else
-      "weather data is unavailable"
+      @location = MapFacade.location(@end_city)
+      @weather = WeatherFacade.weather(@location.latitude, @location.longitude)
+      if @hours == 0
+      {
+        temperature: @weather.current_weather[:temperature],
+        conditions: @weather.current_weather[:conditions]
+      }
+      elsif @hours < 8
+        {
+          temperature: @weather.hourly_weather[@hours][:temperature],
+          conditions: @weather.hourly_weather[@hours][:conditions]
+        }
+      elsif @days < 4
+        {
+          temperature: @weather.daily_weather[@days][:max_temp],
+          conditions: @weather.daily_weather[@days][:conditions]
+        }
+      else
+        "weather data is unavailable"
+      end
     end
-
   end
 end
